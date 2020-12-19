c0b0abab43d3e8cc8f8a277b7e86666046664e7f
Max <max.rose366@gmail.com>
2020-12-19 15:17:13 +0000

    Added day 19

---
+++ Day19.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent_Of_Code_2020
+{
+    public class Day19
+    {
+        public static string[] puzzleInput;
+        private static Dictionary<int, string> rules;
+        private static List<string> data;
+
+        private static void FormatInput()
+        {
+            rules = new();
+            data = new();
+
+            var r = true;
+            foreach (var line in puzzleInput)
+            {
+                if (line == "")
+                {
+                    r = false;
+                    continue;
+                }
+
+                if (r)
+                {
+                    var split = line.Split(":");
+                    var info = split[1].Trim().Replace("\"", "");
+                    rules.Add(int.Parse(split[0]), info);
+                }
+                else
+                    data.Add(line);
+            }
+        }
+        
+        public static void RunDay19Part1()
+        {
+            FormatInput();
+
+            var s = rules[0];
+
+            while (Regex.IsMatch(s, @"\d+"))
+                s = Regex.Replace(s, @"\d+", m => $"({rules[int.Parse(m.Value)]})");
+            
+            var matches = new Regex($"^{s.Replace(" ", "").Replace("\\", "")}$");
+            
+            Console.WriteLine(data.Count(m => matches.IsMatch(m)));
+        }
+
+        public static void RunDay19Part2()
+        {
+            FormatInput();
+            
+            rules[8] = "42 | 42 8";
+            rules[11] = "42 31 | 42 11 31";
+
+            while (true)
+            {
+                var simple = rules.FirstOrDefault(r => !Regex.IsMatch(r.Value, @"\d+") && r.Key != 42 && r.Key != 31);
+
+                if (simple.Equals(default(KeyValuePair<int, string>)))
+                    break;
+
+                foreach (var iKey in rules.Keys.ToArray())
+                    rules[iKey] = Regex.Replace(rules[iKey], $"\\b{simple.Key}\\b", $"({simple.Value})");
+
+                rules.Remove(simple.Key);
+            }
+            
+            rules.TrimExcess();
+
+            rules[31] = rules[31].Replace(" ", "").Replace("\"", "");
+            rules[42] = rules[42].Replace(" ", "").Replace("\"", "");
+
+            var match = new Regex(
+                $"^{rules[0].Replace("8", $"({rules[42]})+").Replace("11", $"(?<A>{rules[42]})+(?<-A>{rules[31]})+").Replace(" ", "")}$");
+            
+            Console.WriteLine(data.Count(m => match.IsMatch(m)));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,42 +12,42 @@ namespace Advent_Of_Code_2020
             // Day2.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day2input.txt");
             // Day2.RunDay2Part1();
             // Day2.RunDay2Part2();
-            
+
             // Day3.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\Day3input.txt");
             // Day3.RunDay3Part1();
             // Day3.RunDay3Part2();
-            
+
             // Day4.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day4inputtest.txt");
             // Day4.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day4input.txt");
             // Day4.GeneratePassports();
             // Day4.RunDay4Part1();
             // Day4.RunDay4Part2();
-            
+
             // Day5.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day5input.txt");
             // Day5.RunDay5Part1();
             // Day5.RunDay5Part2();
-            
+
             // Day6.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day6inputtest.txt");
             // Day6.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day6input.txt");
             // Day6.RunDay6Part1();
             // Day6.RunDay6Part2();
-            
+
             // Day7.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day7inputtest.txt");
             // Day7.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day7inputtest2.txt");
             // Day7.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day7input.txt");
             // Day7.RunDay7Part1();
             // Day7.RunDay7Part2();
-            
+
             // Day8.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day8inputtest.txt");
             // Day8.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day8input.txt");
             // Day8.RunDay8Part1();
             // Day8.RunDay8Part2();
-            
+
             // Day9.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day9inputtest.txt");
             // Day9.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day9input.txt");
             // Console.WriteLine(Day9.RunDay9Part1());
             // Day9.RunDay9Part2();
-            
+
             // Day10.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day10inputtest.txt");
             // Day10.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day10input.txt");
             // Day10.RunDay10Part1();
@@ -57,40 +57,45 @@ namespace Advent_Of_Code_2020
             // Day11.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day11input.txt");
             // Day11.RunDay11Part1();
             // Day11.RunDay11Part2();
-            
+
             // Day12.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day12inputtest.txt");
             // Day12.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day12input.txt");
             // Day12.RunDay12Part1();
             // Day12.RunDay12Part2();
-            
+
             // Day13.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day13inputtest.txt");
             // Day13.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day13input.txt");
             // Day13.RunDay13Part1();
             // Day13.RunDay13Part2();
-            
+
             // Day14.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day14inputtest.txt");
             // Day14.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day14input.txt");
             // Day14.RunDay14Part1();
             // Day14.RunDay14Part2();
-            
+
             // Day15.RunDay15Part1();
             // Day15.RunDay15Part2();
-            
+
             // Day16.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day16inputtest.txt");
             // Day16.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day16input.txt");
             // Day16.FormatInput();
             // Day16.RunDay16Part1();
             // Day16.RunDay16Part2();
-            
+
             // Day17.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day17inputtest.txt");
             // Day17.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day17input.txt");
             // Day17.RunDay17Part1();
             // Day17.RunDay17Part2();
-            
-            Day18.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day18inputtest.txt");
-            Day18.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day18input.txt");
-            Day18.RunDay18Part1();
-            Day18.RunDay18Part2();
+
+            // Day18.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day18inputtest.txt");
+            // Day18.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day18input.txt");
+            // Day18.RunDay18Part1();
+            // Day18.RunDay18Part2();
+
+            Day19.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day19inputtest.txt");
+            // Day19.puzzleInput = System.IO.File.ReadAllLines(@"C:\Users\max\Documents\currentprojects\Advent Of Code 2020\Inputs\day19input.txt");
+            Day19.RunDay19Part1();
+            Day19.RunDay19Part2();
         }
     }
 }
